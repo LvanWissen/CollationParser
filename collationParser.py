db78e417011e0a0bf93834b756c57e97bb521a90
Leon van Wissen <leon@vwissen.nl>
2017-07-20 12:26:48 +0200

    bugfix

--- collationParser.py
+++ collationParser.py
@@ -72,15 +72,22 @@ class CollationParser:
         entries = [m.groupdict() for m in r.finditer(s)]
 
         for e in entries:
+            size = 0
+
             if self.verbose:
                 print()
                 print({k:v for k,v in e.items() if v is not None})
 
             if e['KATERN_START'] and e['KATERN_END']:
 
-                if e['KATERN_START'].startswith('(') and e['KATERN_START'].endswith(')'):
+                brackets = '()[]'
+
+                if e['KATERN_START'][0] in brackets and e['KATERN_START'][-1] in brackets:
                     e['KATERN_START'] = e['KATERN_START'][1:-1]
-                if e['KATERN_END'].startswith('(') and e['KATERN_END'].endswith(')'):
+                elif e['KATERN_START'][0] in brackets and e['KATERN_END'][-1] in brackets:
+                    e['KATERN_START'] = e['KATERN_START'][1:]
+                    e['KATERN_END'] = e['KATERN_END'][:-1]
+                if e['KATERN_END'][0] in brackets and e['KATERN_END'][-1] in brackets:
                     e['KATERN_END'] = e['KATERN_END'][1:-1]
 
                 n_start, s_start = re.findall('(\d+)?([^ ]+)?', e['KATERN_START'])[0]
@@ -165,4 +172,4 @@ if __name__ == "__main__":
    
     cp.parse("π1 †-3†`SUP`12`LO` *`SUP`2`LO` a-e`SUP`12`LO` A-K`SUP`12`LO` `SUP`2`LO`†`SUP`2`LO` χ1 L-2C`SUP`12`LO` 2D`SUP`2`LO` 2χ1 2E-3D`SUP`12`LO` 3E`SUP`4`LO` `SUP`2`LO`A`SUP`2`LO` 3χ1 3F`SUP`8`LO` 3G-4B`SUP`12`LO` 4C`SUP`4`LO` `SUP`2`LO`*`SUP`2`LO` 4χ1 4D`SUP`8`LO` 4E-4Z`SUP`12`LO` 5A`SUP`2`LO` 5χ1 5B-5S`SUP`12`LO` 5T`SUP`4`LO` `SUP`3`LO`*`SUP`2`LO` 6χ1 5V`SUP`8`LO` 5X-6X`SUP`12`LO` 6Y`SUP`4`LO` 6Z`SUP`2`LO` 7χ1 7A-7S`SUP`12`LO` 7T`SUP`6`LO` `SUP`4`LO`*`SUP`2`LO` 8χ1 7V`SUP`6`LO` 7X-8I`SUP`12`LO` 8K`SUP`10`LO` `SUP`5`LO`*`SUP`2`LO` 9χ1 8L`SUP`2`LO` 8M-9D`SUP`12`LO` 9E`SUP`10`LO` 10χ1 11χ1 9G`SUP`2`LO` 9H-9V`SUP`12`LO` 9X`SUP`4`LO` (3E4, 9X4 blank)")
     #cp.parse("*`SUP`4`LO` A-5S`SUP`4`LO` (A)-(2D)`SUP`4`LO` *`SUP`4`LO`(-*4) 2*`SUP`4`LO` A-2F`SUP`4`LO` 2G1 *`SUP`4`LO` A-3R`SUP`4`LO` π`SUP`2`LO` *-4*`SUP`12`LO` 5*`SUP`10`LO` A-2F`SUP`12`LO` (2F11,12 blank) π1 2π`SUP`2`LO` A-2I`SUP`8`LO` (2I7,8 blank) *-2*`SUP`4`LO` A-3R`SUP`4`LO` */2*`SUP`4`LO` 3*-9*`SUP`4`LO` a`SUP`4`LO` b`SUP`4`LO`(-b4) A-5C`SUP`4`LO` (5C4 blank) <***>`SUP`2`LO` A-2E`SUP`4`LO` 2F1 2G-2H`SUP`2`LO` A`SUP`4`LO`(A1+χ1) B-4C`SUP`4`LO` 4D`SUP`6`LO` 2*-3*`SUP`4`LO` a-c`SUP`4`LO` d`SUP`2`LO` e-r`SUP`4`LO` s`SUP`2`LO` t-y`SUP`4`LO` z`SUP`6`LO` (4D6 blank)")
-
+    #cp.parse('[A-B]`SUP`4`LO`')
