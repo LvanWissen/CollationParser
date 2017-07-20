218461e9bdec65234a832c10f5e3bf6bb2a9e5be
Leon van Wissen <leon@vwissen.nl>
2017-07-20 11:29:31 +0200

    add optional logging for package use

--- collationParser.py
+++ collationParser.py
@@ -33,15 +33,17 @@ SOFTWARE.
 
 import re
 
+
 class CollationParser:
     """
     Initializes a CollationParser object. Feed the .parse() function with a
     string to get the amount of folia. It prints the sections. 
     """
 
-    def __init__(self):
+    def __init__(self, verbose=False):
         """
         """
+        self.verbose = verbose
 
         decodestring = "abcdefghiklmnopqrstuvxyz"  # j and w removed
         self.decodestring = decodestring + decodestring.upper()
@@ -57,8 +59,8 @@ class CollationParser:
         katernen = 0
 
 
-
-        print(s)
+        if self.verbose:
+            print(s)
 
         r = re.compile("""
                        (?P<ONGESIGNEERD>(?:(?:\d+)?[χπ]\d{1,2})+)|
@@ -70,8 +72,9 @@ class CollationParser:
         entries = [m.groupdict() for m in r.finditer(s)]
 
         for e in entries:
-            print()
-            print({k:v for k,v in e.items() if v is not None})
+            if self.verbose:
+                print()
+                print({k:v for k,v in e.items() if v is not None})
 
             if e['KATERN_START'] and e['KATERN_END']:
 
@@ -86,22 +89,24 @@ class CollationParser:
                     n_end = 1
                 else:
                     n_end = int(n_end)
-
-                print('n_start:', n_start, 's_start:', s_start)
-                print('n_end:', n_end, 's_end:', s_end)
+                if self.verbose:
+                    print('n_start:', n_start, 's_start:', s_start)
+                    print('n_end:', n_end, 's_end:', s_end)
 
                 if e['KATERN_START'] in self.decodestring and e['KATERN_END'] in self.decodestring:
                     start = self.decodestring.index(e['KATERN_START'].lower())
                     end = self.decodestring.index(e['KATERN_END'].lower())
                     size = end - start + 1
 
-                    print('method1')
+                    if self.verbose:
+                        print('method1')
 
                 elif s_start == s_end and s_start not in self.decodestring:
 
                     size = n_end - n_start
 
-                    print('method2')
+                    if self.verbose:
+                        print('method2')
 
                 elif s_start in self.decodestring and s_end in self.decodestring:
                     start = self.decodestring.index(s_start.lower())
@@ -116,9 +121,11 @@ class CollationParser:
                     else:
                         raise EnvironmentError
 
-                    print('method3')
+                    if self.verbose:
+                        print('method3')
 
-                print(int(e['FORMAAT']), size)
+                if self.verbose:
+                    print(int(e['FORMAAT']), size)
                 folia += int(e['FORMAAT']) * size
 
             elif e['KATERN_START']:
@@ -131,19 +138,22 @@ class CollationParser:
                 folia -= 1
 
             elif e['COMMENTAAR']:
-                print('Commentaar:', e['COMMENTAAR'])
+                if self.verbose:
+                    print('Commentaar:', e['COMMENTAAR'])
 
-            print(folia)
+            if self.verbose:
+                print(folia)
 
-        print("Folia:", folia)
-        print()
+        if self.verbose:
+            print("Folia:", folia)
+            print()
 
 
         return folia
 
 
 if __name__ == "__main__":
-    cp = CollationParser()
+    cp = CollationParser(verbose=True)
    
     cp.parse("π1 †-3†`SUP`12`LO` *`SUP`2`LO` a-e`SUP`12`LO` A-K`SUP`12`LO` `SUP`2`LO`†`SUP`2`LO` χ1 L-2C`SUP`12`LO` 2D`SUP`2`LO` 2χ1 2E-3D`SUP`12`LO` 3E`SUP`4`LO` `SUP`2`LO`A`SUP`2`LO` 3χ1 3F`SUP`8`LO` 3G-4B`SUP`12`LO` 4C`SUP`4`LO` `SUP`2`LO`*`SUP`2`LO` 4χ1 4D`SUP`8`LO` 4E-4Z`SUP`12`LO` 5A`SUP`2`LO` 5χ1 5B-5S`SUP`12`LO` 5T`SUP`4`LO` `SUP`3`LO`*`SUP`2`LO` 6χ1 5V`SUP`8`LO` 5X-6X`SUP`12`LO` 6Y`SUP`4`LO` 6Z`SUP`2`LO` 7χ1 7A-7S`SUP`12`LO` 7T`SUP`6`LO` `SUP`4`LO`*`SUP`2`LO` 8χ1 7V`SUP`6`LO` 7X-8I`SUP`12`LO` 8K`SUP`10`LO` `SUP`5`LO`*`SUP`2`LO` 9χ1 8L`SUP`2`LO` 8M-9D`SUP`12`LO` 9E`SUP`10`LO` 10χ1 11χ1 9G`SUP`2`LO` 9H-9V`SUP`12`LO` 9X`SUP`4`LO` (3E4, 9X4 blank)")
 
